fed8d13662cf03fceb5ee12cb3de7dacabea015e
AngieXD37 <2sebas23tian3@gmail.com>
2022-09-06 18:36:37 -0500

    Formulario

--- Usuario/views.py
+++ Usuario/views.py
@@ -4,18 +4,19 @@ from Usuario.models import *
 from Usuario.forms import *
 
 def crearUsuario(request):
-    usuarios= Ciudadano.objects.all()
-    titulo="Crear usuarios"
-    if request.method =='POST':
-        form=CiudadanoForm(request.POST)
+    titulo_pagina="ciudadano"
+    ciudadanos= Ciudadano.objects.all()
+    if request.method == 'POST':
+        form= CiudadanoForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('crear_usuarios')
+        return redirect('usuarios-aprendiz')
     else:
-        form=CiudadanoForm
+        form = CiudadanoForm()
     context={
-        'usuarios':usuarios,
-        'titulo':titulo
+        "titulo_pagina": titulo_pagina,
+        "ciudadanos": ciudadanos,
+        "form":form
     }
     return render(request, 'usuarios/crearUsuario.html', context)
 
