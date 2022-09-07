7294df0f6f8620276019169734aa7e2301b130e4
AngieXD37 <2sebas23tian3@gmail.com>
2022-09-07 15:05:25 -0500

    Revert "Merge pull request #12 from Sebuchas/Sebastian"
    This reverts commit 437b4897547154702f96d6cf0c33810e8da890f4, reversing
    changes made to 09bfce5936434296bf50af3ecb5ca7c099be002c.

--- cuenta/views.py
+++ cuenta/views.py
@@ -7,11 +7,17 @@ from django.contrib.auth.models import User
 def registrar(request):
 	existe=User.objects.all()
 	if request.method == 'POST':
-		form = UserRegisterForm(request.POST)	
-		if form.is_valid() :	
-			form.save()
-			username = form.cleaned_data['username']
-			messages.success(request, f'Usuario {username} creado correctamente')
+		if len(existe) == 0:
+			form = UserRegisterForm(request.POST)	
+			if form.is_valid() :	
+				form.save()
+				username = form.cleaned_data['username']
+				messages.success(request, f'Usuario {username} creado correctamente')
+				return redirect('index')
+			else:
+				messages.warning(request,'Error solo se puede crear una cuenta')
+		else:
+			messages.success(request,'Error')
 			return redirect('index')
 	else:
 		form = UserRegisterForm()
