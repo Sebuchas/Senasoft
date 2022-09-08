f61091afef637444b28e33dec5e1b19b42e13406
AngieXD37 <2sebas23tian3@gmail.com>
2022-09-08 07:23:50 -0500

    Revert "Merge branch 'Sebastian' into main"
    This reverts commit 6d447bf837094bb8f23f4c7233d498cd7d686073, reversing
    changes made to ddf6d85b0aed06ffd43eef9f857eb6063c4d963d.

--- cuenta/views.py
+++ cuenta/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
-def registrar(request, pk):
+def registrar(request):
+	existe=User.objects.all()
 	if request.method == 'POST':
 		form = UserRegisterForm(request.POST)	
 		if form.is_valid() :	
 			form.save()
 			username = form.cleaned_data['username']
-			
 			messages.success(request, f'Usuario {username} creado correctamente')
-			return redirect('datos-usuario')
+			return redirect('index')
 	else:
 		form = UserRegisterForm()
 	context = { 'form' : form,
